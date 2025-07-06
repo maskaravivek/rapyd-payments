2804eee3c2994c9f9a9d9c74e9362d05aa93c6a0
Vivek Kumar Maskara <maskaravivek@gmail.com>
2025-07-06 13:41:57 -0700

    Update default port in config

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ import { config } from 'dotenv';
 
 config();
 
-export const port = process.env.PORT || 3000;
+export const port = process.env.PORT || 8000;
 export const rapydApiKey = process.env.RAPYD_API_KEY || '';
 export const rapydSecretKey = process.env.RAPYD_SECRET_KEY || '';
